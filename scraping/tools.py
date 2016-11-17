62339e7a157c6fc3bcd9116de8a93563f9cea58c
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-11-17 12:56:25 +0000

    Added APIError and corrections.

--- scraping/tools.py
+++ scraping/tools.py
@@ -4,36 +4,48 @@ except ImportError:
     import xml.etree.ElementTree as ET
 from xml.etree import ElementTree
 from collections import defaultdict
-from password import password
+from scraping.password import password
 
 import json
 import requests
+from ratelimit import *
 
-# standard = 'http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?'
+
+class APIError(Exception):
+    """An API Error Exception"""
+
+    def __init__(self, status):
+        self.status = status
+
+    def __str__(self):
+        return "APIError: status={}".format(self.status)
 
 
-# 1st function: Create the URL
 def create_url_search(parameters, standard):
     """A function which take the parameters given for the search and
-    after combining it to the standard url path for IEEE explorer returns the
+    after combining it to the standard url path for an API and returns the
     url.
     """
-    url = '{}&{}'.format(standard, [_ for _ in parameters])
+    url = standard
+    for i in parameters:
+        url += '&{}'.format(i)
     return url
 
 
-# 2nd function: Get the request
+@rate_limited(1)
 def fetch_xml(url):
     """A function which creates the request to the API and returns the XML
     file.
     """
     response = requests.get(url, stream=True)
-    # response.status_code check error
+
+    if response.status_code != 200:
+        raise APIError(response.status_code)
+
     root = ET.parse(response.raw).getroot()
     return root
 
 
-# 3rd function: Xml to dict
 def xml_to_dict(branch):
     """Branch to dictionary"""
     article = defaultdict()
@@ -43,28 +55,26 @@ def xml_to_dict(branch):
     return article
 
 
-# 4th Transformation to what suites Axelbib
 def iee_to_axelbib(article):
     """A function which takes a dictionary with structure of the IEEE results
     and transform it to a structure understandable by Axelbib"""
     keys = ['abstract', 'author', 'date', 'title', 'journal', 'notes', 'key']
 
     article['author'] = []
-    for i in article['authors'].split(';'):
+    for i in article['authors'].split('; '):
         article['author'].append({'name': i})
     article['date'] = {'year': int(article['py'])}
     article['journal'] = article.pop('pubtitle')
     article['notes'] = article.pop('pdf')
 
-    first_name, last_name = article['author'][0]['name'].split('. ')
+    first_name, last_name = article['author'][0]['name'].split(' ')
     year = article['date']['year']
-    article['key'] = {'key': '{}{}'.format(last_name, year)}
+    article['key'] = {'{}{}'.format(last_name, year)}
 
     post = {key: article[key] for key in keys}
     return post
 
 
-# 5th function: Post to Axelbib
 def post_to_axelbib(post):
     """A function for posting to Axelbib"""
     url = 'http://127.0.0.1:8000/article/'
