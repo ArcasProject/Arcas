9da98c85efe5a68e53efb68402433d4e98b3c121
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-11-19 20:32:53 +0000

    Generalized code.
    Create classes where the old functions live in.

--- scraping/IEEE/main.py
+++ scraping/IEEE/main.py
@@ -17,24 +17,39 @@ from scraping.tools import *
 from docopt import docopt
 
 
-def iee_to_axelbib(article):
-    """A function which takes a dictionary with structure of the IEEE results
-    and transform it to a structure understandable by Axelbib"""
-    keys = ['abstract', 'author', 'date', 'title', 'journal', 'notes', 'key']
+class Ieee(Api):
+    """
 
-    article['author'] = []
-    for i in article['authors'].split('; '):
-        article['author'].append({'name': i})
-    article['date'] = {'year': int(article['py'])}
-    article['journal'] = article.pop('pubtitle')
-    article['notes'] = article.pop('pdf')
+    """
+    def __init__(self):
+        self.standard = 'http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?'
 
-    first_name, last_name = article['author'][0]['name'].split(' ')
-    year = article['date']['year']
-    article['key'] = {'{}{}'.format(last_name, year)}
+    @staticmethod
+    def to_axelbib(article):
+        """A function which takes a dictionary with structure of the IEEE results
+        and transform it to a structure understandable by Axelbib"""
+        keys = ['abstract', 'author', 'date', 'title', 'journal', 'notes', 'key']
 
-    post = {key: article[key] for key in keys}
-    return post
+        article['author'] = []
+        for i in article['authors'].split('; '):
+            article['author'].append({'name': i})
+        article['date'] = {'year': int(article['py'])}
+        article['journal'] = article.pop('pubtitle')
+        article['notes'] = article.pop('pdf')
+
+        first_name, last_name = article['author'][0]['name'].split(' ')
+        year = article['date']['year']
+        article['key'] = {'{}{}'.format(last_name, year)}
+
+        post = {key: article[key] for key in keys}
+        return post
+
+    @staticmethod
+    def parse(root):
+        parents = root.getchildren()
+        for _ in range(2):
+            parents.remove(parents[0])
+        return parents
 
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='IEEE Xplore API Request')
@@ -43,18 +58,7 @@ if __name__ == '__main__':
                   'ab={}'.format(arguments['-ab']), 'py={}'.format(arguments['-py']),
                   'hc={}'.format(arguments['-hc'])]
 
-    standard = 'http://ieeexplore.ieee.org/gateway/ipsSearch.jsp?'
+    api = Ieee()
+    api.run(parameters)
 
-    url = create_url_search(parameters=parameters, standard=standard)
-    root = fetch_xml(url)
-    parents = root.getchildren()
-    for _ in range(2): parents.remove(parents[0])
 
-    for document in parents:
-        article = xml_to_dict(document)
-        post = iee_to_axelbib(article)
-        send = post_to_axelbib(post)
-
-        with open("status_repost", 'a') as textfile:
-            textfile.write('{}{}\n'.format(post['key'], send))
-        textfile.close()
--- scraping/tools.py
+++ scraping/tools.py
@@ -21,45 +21,75 @@ class APIError(Exception):
         return "APIError: status={}".format(self.status)
 
 
-def create_url_search(parameters, standard):
-    """A function which take the parameters given for the search and
-    after combining it to the standard url path for an API and returns the
-    url.
-    """
-    url = standard
-    for i in parameters:
-        url += '&{}'.format(i)
-    return url
+class Api():
+    """A class ..."""
+    def __init__(self):
+        self.standard = None
+
+    def create_url_search(self, parameters):
+        """A function which take the parameters given for the search and
+        after combining it to the standard url path for an API and returns the
+        url.
+        """
+        url = self.standard
+        for i in parameters:
+            url += '&{}'.format(i)
+        return url
+
+    @staticmethod
+    @rate_limited(1)
+    def fetch_xml(url):
+        """A function which creates the request to the API and returns the XML
+        file.
+        """
+        response = requests.get(url, stream=True)
+
+        if response.status_code != 200:
+            raise APIError(response.status_code)
+
+        root = ET.parse(response.raw).getroot()
+        return root
+
+    @staticmethod
+    def xml_to_dict(branch):
+        """Branch to dictionary"""
+        article = defaultdict()
+        for at in branch.iter():
+            article.update({at.tag: at.text})
+
+        return article
+
+    @staticmethod
+    def post_to_axelbib(post):
+        """A function for posting to Axelbib"""
+        url = 'http://127.0.0.1:8000/article/'
+        headers = {'Content-Type': 'application/json'}
+
+        r = requests.post(url, data=json.dumps(post),
+                          auth=('nikoleta', password), headers=headers)
+        return r.status_code
 
 
-@rate_limited(1)
-def fetch_xml(url):
-    """A function which creates the request to the API and returns the XML
-    file.
-    """
-    response = requests.get(url, stream=True)
+    @staticmethod
+    def to_axelbib(article):
+        pass
 
-    if response.status_code != 200:
-        raise APIError(response.status_code)
+    @staticmethod
+    def parse(root):
+        pass
 
-    root = ET.parse(response.raw).getroot()
-    return root
+    def run(self, parameters, filename="status_report"):
+        """
+        Runing...
+        """
+        url = self.create_url_search(parameters=parameters)
+        root = self.fetch_xml(url)
+        parents = self.parse(root)
 
+        for document in parents:
+            article = self.xml_to_dict(document)
+            post = self.to_axelbib(article)
+            send = self.post_to_axelbib(post)
 
-def xml_to_dict(branch):
-    """Branch to dictionary"""
-    article = defaultdict()
-    for at in branch.iter():
-        article.update({at.tag: at.text})
-
-    return article
-
-
-def post_to_axelbib(post):
-    """A function for posting to Axelbib"""
-    url = 'http://127.0.0.1:8000/article/'
-    headers = {'Content-Type': 'application/json'}
-
-    r = requests.post(url, data=json.dumps(post),
-                      auth=('nikoleta', password), headers=headers)
-    return r.status_code
+            with open(filename, 'a') as textfile:
+                textfile.write('{}{}\n'.format(post['key'], send))
