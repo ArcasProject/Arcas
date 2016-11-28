e8126ef0551b62f20607afc8e68224e5c7cc1ba7
Nikoleta Glynatsi <GlynatsiNE@cardiff.ac.uk>
2016-11-28 18:04:31 +0000

    Added test for to_json for both apis.

--- scraping/tests.py
+++ scraping/tests.py
@@ -4,6 +4,49 @@ import requests
 import requests_mock
 from xml.etree.ElementTree import Element
 from scraping.tools import Api
+from scraping.IEEE.main import Ieee
+from scraping.arXiv.main import Arxiv
+
+arxiv_entry = dict(
+                   {'{http://arxiv.org/schemas/atom}affiliation': 'A',
+                    '{http://arxiv.org/schemas/atom}author': 'B',
+                    '{http://arxiv.org/schemas/atom}category': None,
+                    '{http://arxiv.org/schemas/atom}comment': '8 pages',
+                    '{http://arxiv.org/schemas/atom}doi': '10.10.10',
+                    '{http://arxiv.org/schemas/atom}entry': '132',
+                    '{http://arxiv.org/schemas/atom}id': 'http://arxiv.org/',
+                    '{http://arxiv.org/schemas/atom}journal_ref': '',
+                    '{http://arxiv.org/schemas/atom}link': None,
+                    '{http://arxiv.org/schemas/atom}name': 'C',
+                    '{http://arxiv.org/schemas/atom}primary_category': None,
+                    '{http://arxiv.org/schemas/atom}published': '2016',
+                    '{http://arxiv.org/schemas/atom}summary': 'Summary',
+                    '{http://arxiv.org/schemas/atom}title': 'Title',
+                    '{http://arxiv.org/schemas/atom}updated': '2016'})
+
+ieee_entry = dict(
+                  {'rank': '1',
+                   'title': "Title",
+                   'authors': 'A',
+                   'affiliations': 'B',
+                   'term': 'C',
+                   'term': 'D',
+                   'pubtitle': 'E',
+                   'punumber': '1',
+                   'publisher': 'IEEE',
+                   'py': '2016',
+                   'spage': '1',
+                   'epage': '2',
+                   'abstract': "Abstract",
+                   'issn': '1234-5678',
+                   'doi': '10.10',
+                   'publicationId': '1234',
+                   'partnum': '1234',
+                   'mdurl': 'http://ieeexplore.ieee.org',
+                   'pdf': 'http://ieeexplore.ieee.org/'})
+
+keys = ['key', 'unique_key', 'title', 'abstract', 'author', 'date', 'journal',
+        'pages', 'labels', 'read', 'key_word', 'provelance', 'list_strategies']
 
 
 class TestTools(unittest.TestCase):
@@ -36,3 +79,21 @@ class TestTools(unittest.TestCase):
         dummy_dict = self.api.xml_to_dict(root)
         self.assertEqual(type(dummy_dict), collections.defaultdict)
 
+
+class TestArxiv(unittest.TestCase):
+
+    def setUp(self):
+        self.api = Arxiv()
+
+    def test_to_json(self):
+        post = self.api.to_json(arxiv_entry)
+        self.assertEqual(sorted(post.keys()), sorted(keys))
+
+
+class TestIEEE(unittest.TestCase):
+    def setUp(self):
+        self.api = Ieee()
+
+    def test_to_json(self):
+        post = self.api.to_json(ieee_entry)
+        self.assertEqual(sorted(post.keys()), sorted(keys))
